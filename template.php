55c1f1966b5a917d4bcedaf447ca0ded859b247c
Evan <markevancoc@gmail.com>
2024-11-24 14:02:28 +0800

    added template

---
+++ template.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html data-theme="autumn" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome</title>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+    <script>
+        tailwind.config = {
+            daisyui: {
+                themes: ["cupcake"],
+            },
+            theme: {
+                extend: {
+                    colors: {
+                        clifford: '#da373d'z
+                    }
+                }
+            },
+            plugins: [require("@tailwindcss/typography"), require("daisyui")],
+        }
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+
+</body>
