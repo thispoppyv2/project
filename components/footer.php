93dac2029882af2d5693cfeea3923dd0f679239a
Evan <markevancoc@gmail.com>
2024-11-24 14:00:43 +0800

    added footer

--- components/footer.php
+++ components/footer.php
@@ -6,18 +6,9 @@ echo '
     HOTEL DE LUNA
 </div>
   <nav>
-    <h6 class="footer-title">Services</h6>
-    <a class="link link-hover">Branding</a>
-    <a class="link link-hover">Design</a>
-    <a class="link link-hover">Marketing</a>
-    <a class="link link-hover">Advertisement</a>
-  </nav>
-  <nav>
     <h6 class="footer-title">Company</h6>
     <a class="link link-hover">About us</a>
     <a class="link link-hover">Contact</a>
-    <a class="link link-hover">Jobs</a>
-    <a class="link link-hover">Press kit</a>
   </nav>
   <nav>
     <h6 class="footer-title">Legal</h6>
