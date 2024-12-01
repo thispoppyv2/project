ff4a3034373713d0c857ac95964ffce416a34d68
aniban <nathankendricklopez@gmail.com>
2024-12-01 01:56:03 -0800

    aksdka

--- backend/add-book.php
+++ backend/add-book.php
@@ -3,14 +3,13 @@
 
 global $conn;
 include '../backend/connection.php';
-$contact_number = $_POST["contact_number"];
-$booked_hotel = $_POST ["booked_hotel"];
+$customer_number = $_POST["customer_number"];
 $customer_name = $_POST ["customer_name"];
 $date_arriving = $_POST ["date_arriving"];
 $date_departure = $_POST ["date_departure"];
 $room_type = $_POST ["room_type"];
 
-$sql = "INSERT INTO bookings SET contact_number='$contact_number',booked_hotel='$booked_hotel',customer_name='$customer_name', date_arriving='$date_arriving', date_departure='$date_departure', room_type='$room_type'";
+$sql = "INSERT INTO bookings SET customer_number='$customer_number',customer_name='$customer_name', date_arriving='$date_arriving', date_departure='$date_departure', room_type='$room_type'";
 
 
 if (mysqli_query($conn, $sql)) {
